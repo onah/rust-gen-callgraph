a80a595a055be32b42b7ecb46ad222c2e9b60446
Hano Hiroyuki <onahdwahan@gmail.com>
2021-09-25 15:10:21 +0900

    add method etc

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -5,6 +5,7 @@ use std::error;
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
+//use syn::spanned::Spanned;
 
 pub fn analyze(filename: PathBuf) -> Result<Vec<CallInfo>, Box<dyn error::Error>> {
     let mut ana = Analyzer::new();
@@ -46,14 +47,42 @@ impl Analyzer {
         let syntax = syn::parse_file(&src)?;
 
         for item in syntax.items {
-            if let syn::Item::Fn(item_fn) = item {
-                self.current_function = Some(item_fn.sig.ident.to_string());
-                //println!("{:?}", item_fn.sig.ident.to_string());
-                self.walk_stmt(item_fn.block.stmts);
+            match item {
+                syn::Item::Fn(item_fn) => self.walk_item_fn(item_fn),
+                syn::Item::Impl(item_impl) => self.walk_item_impl(item_impl),
+                _ => (),
             }
         }
 
         Ok(())
+    }
+
+    fn walk_item_fn(&mut self, item_fn: syn::ItemFn) {
+        self.current_function = Some(item_fn.sig.ident.to_string());
+        self.walk_stmt(item_fn.block.stmts);
+    }
+
+    fn walk_item_impl(&mut self, item_impl: syn::ItemImpl) {
+        match *(item_impl.self_ty) {
+            syn::Type::Path(type_path) => {
+                println!("debug");
+                println!("{}", punctuated_to_string(type_path.path.segments));
+            }
+            _ => (),
+        }
+
+        for item in item_impl.items {
+            match item {
+                syn::ImplItem::Method(impl_item_method) => {
+                    self.walk_impl_item_method(impl_item_method)
+                }
+                _ => (),
+            }
+        }
+    }
+
+    fn walk_impl_item_method(&mut self, method: syn::ImplItemMethod) {
+        self.walk_stmt(method.block.stmts);
     }
 
     fn walk_stmt(&mut self, items: Vec<syn::Stmt>) {
@@ -78,6 +107,10 @@ impl Analyzer {
                     punctuated_to_string(expr_path.path.segments),
                     self.current_function.clone().unwrap(),
                 ));
+            }
+            syn::Expr::MethodCall(expr_methodcall) => {
+                //println!("{:?}", expr_methodcall.span().source_file());
+                println!("{:?}", expr_methodcall.method);
             }
 
             _ => (),
