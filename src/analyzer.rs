4b60b72d3afe9bafb440ecb755cd7dfd57eb9851
Hano Hiroyuki <onahdwahan@gmail.com>
2021-10-24 19:44:36 +0900

    add Status struct

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -13,20 +13,33 @@ pub fn analyze(filename: PathBuf) -> Result<Vec<CallInfo>, Box<dyn error::Error>
     Ok(analyzer.calls)
 }
 
+struct Status {
+    current_function: Option<String>,
+    current_impl: Option<String>,
+    filename: Option<PathBuf>,
+}
+
+impl Status {
+    pub fn new() -> Status {
+        Status {
+            current_function: None,
+            current_impl: None,
+            filename: None,
+        }
+    }
+}
+
 struct Analyzer {
     calls: Vec<CallInfo>,
-    //current_function: Option<String>,
-    item_impl: Option<String>,
+    status: Status,
 }
 
 impl Analyzer {
     pub fn new() -> Analyzer {
         let calls: Vec<CallInfo> = Vec::new();
-        Analyzer {
-            calls,
-            //current_function: None,
-            item_impl: None,
-        }
+        let status = Status::new();
+
+        Analyzer { calls, status }
     }
 
     pub fn run(&mut self, filename: PathBuf) -> Result<(), Box<dyn error::Error>> {
@@ -34,8 +47,12 @@ impl Analyzer {
         let mut src = String::new();
         file.read_to_string(&mut src)?;
 
+        self.status.filename = Some(filename);
+
         let syntax = syn::parse_file(&src)?;
         self.walk_file(syntax);
+
+        self.status.filename = None;
 
         Ok(())
     }
@@ -51,59 +68,76 @@ impl Analyzer {
     }
 
     fn walk_item_fn(&mut self, item_fn: syn::ItemFn) {
-        let fn_name = item_fn.sig.ident.to_string();
-        self.walk_block(*item_fn.block, &fn_name);
+        let mut caller = String::new();
+        caller.push_str(
+            self.status
+                .filename
+                .clone()
+                .unwrap()
+                .file_stem()
+                .unwrap()
+                .to_str()
+                .unwrap(),
+        );
+        caller.push_str("::");
+        caller.push_str(&item_fn.sig.ident.to_string());
+
+        //let fn_name = item_fn.sig.ident.to_string();
+        //self.status.current_function = Some(fn_name);
+
+        self.status.current_function = Some(caller);
+        self.walk_block(*item_fn.block);
+        self.status.current_function = None;
     }
 
     fn walk_item_impl(&mut self, item_impl: syn::ItemImpl) {
-        match *(item_impl.self_ty) {
-            syn::Type::Path(type_path) => {
-                self.item_impl = Some(punctuated_to_string(type_path.path.segments));
-            }
-            _ => (),
+        if let syn::Type::Path(type_path) = *(item_impl.self_ty) {
+            self.status.current_impl = Some(punctuated_to_string(type_path.path.segments));
         }
 
         for item in item_impl.items {
             match item {
                 syn::ImplItem::Method(impl_item_method) => {
-                    self.walk_impl_item_method(impl_item_method)
+                    self.walk_impl_item_method(impl_item_method);
                 }
                 _ => (),
             }
         }
+        self.status.current_impl = None;
     }
 
-    fn walk_block(&mut self, block: syn::Block, fn_name: &String) {
+    fn walk_block(&mut self, block: syn::Block) {
         for stmt in block.stmts {
-            self.walk_stmt(stmt, fn_name);
+            self.walk_stmt(stmt);
         }
     }
 
     fn walk_impl_item_method(&mut self, method: syn::ImplItemMethod) {
-        // TODO: delete current_function
         let mut caller = String::new();
-        caller.push_str(&(self.item_impl.clone().unwrap()));
+        caller.push_str(&self.status.current_impl.clone().unwrap());
         caller.push_str("::");
         caller.push_str(&(method.sig.ident.to_string()));
 
-        //self.current_function = Some(caller);
-        self.walk_block(method.block, &caller);
+        self.status.current_function = Some(caller);
+        self.walk_block(method.block);
+        self.status.current_function = None;
     }
 
-    fn walk_stmt(&mut self, stmt: syn::Stmt, caller: &String) {
+    fn walk_stmt(&mut self, stmt: syn::Stmt) {
         match stmt {
-            syn::Stmt::Expr(expr) => self.walk_expr(expr, caller),
-            syn::Stmt::Semi(expr, _semi) => self.walk_expr(expr, caller),
+            syn::Stmt::Expr(expr) => self.walk_expr(expr),
+            syn::Stmt::Semi(expr, _semi) => self.walk_expr(expr),
             _ => (),
         }
     }
 
-    fn walk_expr(&mut self, item: syn::Expr, caller: &String) {
+    fn walk_expr(&mut self, item: syn::Expr) {
         match item {
             syn::Expr::Call(expr_call) => {
-                self.walk_expr(*expr_call.func, caller);
+                self.walk_expr(*expr_call.func);
             }
             syn::Expr::Path(expr_path) => {
+                let caller = self.status.current_function.clone().unwrap();
                 self.push_callinfo(punctuated_to_string(expr_path.path.segments), caller);
             }
 
@@ -113,7 +147,13 @@ impl Analyzer {
                 match *(expr_methodcall.receiver) {
                     syn::Expr::Path(expr_path) => {
                         if "self" == punctuated_to_string(expr_path.path.segments) {
-                            method_name.push_str(&(self.item_impl.clone().unwrap()));
+                            method_name.push_str(
+                                &(self
+                                    .status
+                                    .current_impl
+                                    .clone()
+                                    .unwrap_or(String::from("NonImpl"))),
+                            );
                             method_name.push_str("::");
                         }
                     }
@@ -121,29 +161,32 @@ impl Analyzer {
                 }
 
                 method_name.push_str(&(expr_methodcall.method.to_string()));
+                let caller = self.status.current_function.clone().unwrap();
                 self.push_callinfo(method_name, caller);
             }
-            syn::Expr::If(expr_if) => {
-                self.walk_block(expr_if.then_branch, caller);
-            }
-            syn::Expr::ForLoop(expr_forloop) => {
-                self.walk_block(expr_forloop.body, caller);
+            syn::Expr::If(expr_if) => self.walk_block(expr_if.then_branch),
+
+            syn::Expr::ForLoop(expr_forloop) => self.walk_block(expr_forloop.body),
+
+            syn::Expr::Block(expr_block) => self.walk_block(expr_block.block),
+
+            syn::Expr::Match(expr_match) => {
+                for arm in expr_match.arms {
+                    self.walk_expr(*arm.body);
+                }
             }
 
             _ => (),
         }
     }
 
-    fn push_callinfo(&mut self, callee: String, caller: &String) {
+    fn push_callinfo(&mut self, callee: String, caller: String) {
         //let caller = self
         //    .current_function
         //    .clone()
         //    .unwrap_or(String::from("NonData"));
 
-        let callinfo = CallInfo {
-            callee,
-            caller: caller.to_string(),
-        };
+        let callinfo = CallInfo { callee, caller };
         self.calls.push(callinfo);
     }
 }
