4fabd112dbd059b4509a7366f69a457cb5e12b51
HANO Hiroyuki <onahdwahan@gmail.com>
2023-03-29 07:32:30 +0900

    refactoring get_module_name

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -8,7 +8,6 @@ use self::callgraph::AnalyzerCallGraph;
 use self::function::AnalyzerFunction;
 use crate::call_data::CallInfo;
 use std::error;
-use std::ffi::OsStr;
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
@@ -30,7 +29,7 @@ pub fn analyze(files: &Vec<PathBuf>) -> Result<Vec<CallInfo>, Box<dyn error::Err
     }
 
     for filename in files {
-        let module_name = get_module_name(filename)?;
+        let module_name = name_resolver::get_module_name(filename)?;
         let mut analyzer = AnalyzerCallGraph::new(module_name);
 
         let mut file = File::open(filename)?;
@@ -43,43 +42,6 @@ pub fn analyze(files: &Vec<PathBuf>) -> Result<Vec<CallInfo>, Box<dyn error::Err
         let mut calls = analyzer.get_callinfo();
 
         result.append(&mut calls);
-    }
-
-    Ok(result)
-}
-
-fn get_module_name(file_name: &PathBuf) -> Result<String, Box<dyn error::Error>> {
-    let result;
-
-    if file_name == OsStr::new("./src/lib.rs") {
-        let mut f = File::open("Cargo.toml")?;
-        let mut contents = String::new();
-        f.read_to_string(&mut contents)?;
-
-        let values = contents.parse::<toml::Value>()?;
-        let project_name = values["package"]["name"].as_str().unwrap_or("");
-        result = String::from(project_name);
-    } else if file_name.file_name().unwrap_or_else(|| OsStr::new("")) == OsStr::new("lib.rs") {
-        let mut filename2 = file_name.clone();
-        filename2.pop();
-        if filename2.file_name().unwrap() == OsStr::new("src") {
-            filename2.pop();
-            {
-                // TODO Copy Code Refactoring
-                let mut f = File::open(format!("{}/Cargo.toml", filename2.to_str().unwrap()))?;
-                let mut contents = String::new();
-                f.read_to_string(&mut contents)?;
-
-                let values = contents.parse::<toml::Value>()?;
-                let project_name = values["package"]["name"].as_str().unwrap();
-
-                result = String::from(project_name);
-            }
-        } else {
-            result = filename2.file_stem().unwrap().to_str().unwrap().to_string();
-        }
-    } else {
-        result = file_name.file_stem().unwrap().to_str().unwrap().to_string();
     }
 
     Ok(result)
--- src/analyzer/name_resolver.rs
+++ src/analyzer/name_resolver.rs
@@ -1,3 +1,10 @@
+use std::error;
+use std::ffi::OsString;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use thiserror::Error;
+
 pub struct VariableDefine {
     name: String,
     // TODO: Option ha huyou ??
@@ -19,4 +26,46 @@ impl VariableDefine {
     pub fn variable_type(&self) -> Option<String> {
         self.variable_type.clone()
     }
+}
+
+#[derive(Error, Debug)]
+pub enum ErrorKind {
+    #[error("Invalid location from lib.rs")]
+    InvalidLocLibRs,
+    #[error("Invalid package name in cargo.toml")]
+    InvalidPackageName,
+    #[error("Invalid filename")]
+    InvalidFilename,
+}
+
+/// Check the module name to which the file belongs.
+/// If filename is lib.rs, then we check the Cargo.toml
+pub fn get_module_name(file_path: &PathBuf) -> Result<String, Box<dyn error::Error>> {
+    let file_name = file_path.file_name().ok_or(ErrorKind::InvalidFilename)?;
+    if file_name == OsString::from("lib.rs") {
+        // If the file name is lib.rs, there is cargo.toml up two.
+        let parent_dir = file_path.parent().ok_or(ErrorKind::InvalidLocLibRs)?;
+        let cargo_dir = parent_dir.parent().ok_or(ErrorKind::InvalidLocLibRs)?;
+        let cargo_file = cargo_dir.join("Cargo.toml");
+
+        return get_project_name_from_cargo_toml(&cargo_file);
+    }
+
+    let result = file_path.file_stem().ok_or(ErrorKind::InvalidFilename)?;
+    let result = result.to_str().ok_or(ErrorKind::InvalidFilename)?;
+
+    Ok(result.to_string())
+}
+
+fn get_project_name_from_cargo_toml(cargo_file: &PathBuf) -> Result<String, Box<dyn error::Error>> {
+    let mut f = File::open(cargo_file)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    let values = contents.parse::<toml::Value>()?;
+    let project_name = values["package"]["name"]
+        .as_str()
+        .ok_or(ErrorKind::InvalidPackageName)?;
+
+    Ok(String::from(project_name))
 }
