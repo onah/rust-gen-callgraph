2e9978bfa7eb7bd1b1f2b8f6236865720d2a7358
HANO Hiroyuki <onahdwahan@gmail.com>
2024-06-07 21:07:57 +0900

    refactoring get_project_name

--- src/analyzer/project_infomation.rs
+++ src/analyzer/project_infomation.rs
@@ -18,7 +18,11 @@ pub struct ProjectInfomaion {
 impl ProjectInfomaion {
     pub fn new(project_path: &PathBuf) -> Result<ProjectInfomaion, Box<dyn error::Error>> {
         let cargo_file = project_path.join("Cargo.toml");
-        let project_name = Self::get_project_name_from_cargo_toml(&cargo_file)?;
+        let mut f = File::open(&cargo_file)?;
+        let mut contents = String::new();
+        f.read_to_string(&mut contents)?;
+
+        let project_name = Self::get_project_name_from_cargo_toml(&contents)?;
         Ok(ProjectInfomaion { project_name })
     }
 
@@ -27,17 +31,44 @@ impl ProjectInfomaion {
     }
 
     fn get_project_name_from_cargo_toml(
-        cargo_file: &PathBuf,
+        cargo_toml_content: &str,
     ) -> Result<String, Box<dyn error::Error>> {
-        let mut f = File::open(cargo_file)?;
-        let mut contents = String::new();
-        f.read_to_string(&mut contents)?;
-
-        let values = contents.parse::<toml::Value>()?;
+        let values = cargo_toml_content.parse::<toml::Value>()?;
+        // TODO: Handle error
+        // panic if package name is not found
         let project_name = values["package"]["name"]
             .as_str()
             .ok_or(ErrorKind::InvalidPackageName)?;
 
         Ok(String::from(project_name))
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_project_name_from_cargo_toml() {
+        let cargo_toml_content = r#"
+            [package]
+            name = "test"
+            version = "0.1.0"
+        "#;
+        let project_name = ProjectInfomaion::get_project_name_from_cargo_toml(cargo_toml_content)
+            .expect("Failed to get project name from cargo.toml");
+        assert_eq!(project_name, "test");
+    }
+
+    /*
+    #[test]
+    fn test_get_project_name_from_cargo_toml_invalid_package_name() {
+        let cargo_toml_content = r#"
+            [package]
+            version = "0.1.0"
+        "#;
+        let project_name = ProjectInfomaion::get_project_name_from_cargo_toml(cargo_toml_content);
+        assert!(project_name.is_err());
+    }
+    */
 }
